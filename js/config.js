541c10ae3a8350ac1e3940db3489757b86d655e2
vijju004 <ametivijaydatta@gmail.com>
2024-08-16 17:51:38 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ window._config = {
         region: 'ap-south-1' 
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://flnpuaui24.execute-api.ap-south-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
 
